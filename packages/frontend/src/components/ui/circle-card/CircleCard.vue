30de3bb6e712167b2ec9d89125d2f0e41721a053
mereditsommer <meredit.sommer@youengineering.ch>
2023-10-28 21:34:04 +0200

    add props to circle card

--- packages/frontend/src/components/ui/circle-card/CircleCard.vue
+++ packages/frontend/src/components/ui/circle-card/CircleCard.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import { Cog6ToothIcon, ArrowRightIcon } from '@heroicons/vue/24/outline'
+
+interface Props {
+  groupName: string
+  id: string
+}
+
+defineProps<Props>()
 </script>
 
 <template>
@@ -7,7 +14,7 @@ import { Cog6ToothIcon, ArrowRightIcon } from '@heroicons/vue/24/outline'
     class="font-yeseva text-6 flex justify-between rounded-2xl bg-white p-6 drop-shadow-lg"
   >
     <span class="flex gap-x-4">
-      Groupname
+      {{ $props.groupName }}
       <cog-6-tooth-icon class="w-6"></cog-6-tooth-icon>
     </span>
     <arrow-right-icon class="w-6"></arrow-right-icon>
--- packages/frontend/src/pages/InnerCircles.vue
+++ packages/frontend/src/pages/InnerCircles.vue
@@ -6,9 +6,9 @@ import QuestionCard from '@/components/ui/question-card/QuestionCard.vue'
 <template>
   <main>
     <div class="inlay flex flex-col gap-y-6">
-      <CircleCard></CircleCard>
-      <CircleCard></CircleCard>
-      <CircleCard></CircleCard>
+      <CircleCard id="1" group-name="Friends"></CircleCard>
+      <CircleCard id="2" group-name="Family"></CircleCard>
+      <CircleCard id="3" group-name="Nadia"></CircleCard>
       <QuestionCard></QuestionCard>
     </div>
   </main>
