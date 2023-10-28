3b3ac3e4e7cd6755dd93b71f6a18a0c5f082855d
cyrilkym <cyril.kym@youengineering.ch>
2023-10-28 17:18:54 +0200

    implemented vue router

---
+++ packages/frontend/src/components/ui/communityCard/CommunityCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps({
+    title: { type: String, required: true},
+    content: { type: String, required: true},
+    variant: String,
+    numberOfComment: { type: Number }
+  });
+</script>
+<template>
+  <div class="drop-shadow">
+    <h2>{{title}}</h2>
+    <p>{{content}}</p>
+    <p>Number of Commits: {{numberOfComment}}</p>
+  </div>
+</template>
--- packages/frontend/src/pages/Community.vue
+++ packages/frontend/src/pages/Community.vue
@@ -1,8 +1,12 @@
 <script setup>
+import CommunityCard from "@/components/ui/communityCard/CommunityCard.vue";
 </script>
 
 <template>
   <main>
     <h1>Community View</h1>
+    <div>
+      <CommunityCard title="Title" number-of-comment="20" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy sed diam nonumy..."/>
+    </div>
   </main>
 </template>
