58571f47b74bbb5e5d1550b3af83eaf1b548846f
cyrilkym <cyril.kym@youengineering.ch>
2023-10-28 20:33:23 +0200

    added text styles

--- packages/frontend/src/pages/Community.vue
+++ packages/frontend/src/pages/Community.vue
@@ -5,7 +5,7 @@ import CommunityCard from "@/components/ui/communityCard/CommunityCard.vue";
 <template>
   <main>
     <h1>Community View</h1>
-    <div>
+    <div class="flex flex-col gap-y-4">
       <CommunityCard :number-of-comment="20" title="Title" variant="surprised" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy sed diam nonumy..."/>
       <CommunityCard title="Title" variant="sad" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy sed diam nonumy..."/>
       <CommunityCard :number-of-comment="25" title="Title" variant="surprised" content="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy sed diam nonumy..."/>
