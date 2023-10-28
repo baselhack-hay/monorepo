25fc6a659168167f854bbe09b327c72a37b4dcbb
nadiakramer <nadia.kramer@youengineering.ch>
2023-10-28 23:20:54 +0200

    fix build

--- packages/frontend/src/pages/Login.vue
+++ packages/frontend/src/pages/Login.vue
@@ -1,33 +1,7 @@
 <script setup lang="ts">
-import {useForm} from 'vee-validate'
-import {toTypedSchema} from '@vee-validate/zod'
-import * as z from 'zod'
 
 import {Button} from '@/components/ui/button'
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form'
-import {Input} from '@/components/ui/input'
 import axios from "axios";
-
-const formSchema = toTypedSchema(z.object({
-  username: z.string().min(4).max(20),
-  password: z.string().min(4).max(20),
-}))
-
-// const { handleSubmit } = useForm({
-//   validationSchema: formSchema,
-// })
-
-const onSubmit = (values) => {
-  axios.post('http://localhost:3000/user/signup', {
-    username: values.username,
-    password: values.password
-  })
-}
 
 const test = () => {
   axios.post('http://localhost:3000/user/signup', {
@@ -45,26 +19,17 @@ const test = () => {
       </div>
       <div class="flex flex-col gap-6 items-center">
         <div class="bg-white rounded-2xl bg-white p-6 drop-shadow-md w-full">
-          <form @submit="onSubmit" class="flex flex-col gap-4">
-            <FormField v-slot="{ username }" name="username">
-              <FormItem>
-                <FormControl class="border-none">
-                  <v-text-field variant="underlined"  placeholder="Username" v-bind="username"/>
-                </FormControl>
-                <FormMessage/>
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ password }" name="password">
-              <FormItem>
-                <FormControl class="border-none">
-                  <v-text-field variant="underlined" type="password" placeholder="Passwort" v-bind="password"/>
-                </FormControl>
-                <FormMessage/>
-              </FormItem>
-            </FormField>
+          <form class="flex flex-col gap-4">
+            <div>
+              <v-text-field variant="underlined" placeholder="Username"/>
+            </div>
+            <div>
+              <v-text-field variant="underlined" type="password" placeholder="Passwort"/>
+            </div>
           </Form>
         </div>
-        <Button @click="test()" variant="outline" class="w-fit px-16 py-6 drop-shadow-md rounded-full text-5 bg-white border-none">
+        <Button @click="test()" variant="outline"
+                class="w-fit px-16 py-6 drop-shadow-md rounded-full text-5 bg-white border-none">
           Submit
         </Button>
       </div>
