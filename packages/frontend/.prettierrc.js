5e9d6c1dbb1ec76b245c34406cbdc53accf2b5ed
megiSW <marco.inniger@sitewerk.ch>
2023-10-28 13:37:16 +0200

    feat: add tailwind and prettier settings

---
+++ packages/frontend/.prettierrc.js
@@ -0,0 +1,8 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "none",
+  tailwindConfig: "./tailwind.config.ts",
+  tailwindFunctions: ["clsx", "cva", "cn"],
+  plugins: ["prettier-plugin-tailwindcss"]
+}
