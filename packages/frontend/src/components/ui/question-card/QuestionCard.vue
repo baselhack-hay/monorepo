e2b6ff499191900f8db0834f7bf2f6a7fbca3f17
leonden <leon.hochwimmer@coop.ch>
2023-10-28 23:39:34 +0200

    Adjust font

--- packages/frontend/src/components/ui/question-card/QuestionCard.vue
+++ packages/frontend/src/components/ui/question-card/QuestionCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="font-yeseva rounded-2xl bg-white p-6 drop-shadow-md">
-    <p class="text-6 mb-1">Question</p>
-    <p class="text-4">
+  <div class="rounded-2xl bg-white p-6 font-yeseva drop-shadow-md">
+    <p class="mb-1 text-6">Question</p>
+    <p class="font-poppins text-4">
       Du bist sowohl geografisch als auch finanziell unabhängig. Wie und wo
       lebst du?
     </p>
