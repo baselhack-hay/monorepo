f05a9460324b205029e74128984d5a763786a02e
leonden <leon.hochwimmer@coop.ch>
2023-10-28 17:19:52 +0200

    Adjust font

--- packages/frontend/src/components/ui/header/Header.vue
+++ packages/frontend/src/components/ui/header/Header.vue
@@ -4,6 +4,6 @@
   <header
     class="grid place-items-center rounded-b-2xl bg-white p-4 backdrop-blur-md"
   >
-    <h2 class="font-veseva text-3xl font-semibold text-black">HAY</h2>
+    <h2 class="font-yeseva text-3xl font-semibold text-black">HAY</h2>
   </header>
 </template>
