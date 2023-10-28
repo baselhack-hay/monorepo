24d950aef757f0bee42150a3e5709a6f84512759
samuellupica <samuel.lupica@students.fhnw.ch>
2023-10-28 23:06:56 +0200

    style login page

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -2,12 +2,13 @@
 import Container from './components/Container.vue'
 import Header from './components/ui/header/Header.vue'
 import AppBar from "@/components/ui/app-bar/AppBar.vue";
+import router from "@/router";
 </script>
 
 <template>
-  <Header></Header>
+  <Header v-if="router.currentRoute.value.fullPath !== '/login'"></Header>
   <Container>
     <router-view></router-view>
   </Container>
-  <AppBar></AppBar>
+  <AppBar v-if="router.currentRoute.value.fullPath !== '/login'"></AppBar>
 </template>
--- packages/frontend/src/pages/Login.vue
+++ packages/frontend/src/pages/Login.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { useForm } from 'vee-validate'
-import { toTypedSchema } from '@vee-validate/zod'
+import {useForm} from 'vee-validate'
+import {toTypedSchema} from '@vee-validate/zod'
 import * as z from 'zod'
 
-import { Button } from '@/components/ui/button'
+import {Button} from '@/components/ui/button'
 import {
   FormControl,
   FormField,
   FormItem,
   FormMessage,
 } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import {Input} from '@/components/ui/input'
 import axios from "axios";
 
 const formSchema = toTypedSchema(z.object({
@@ -38,27 +38,37 @@ const test = () => {
 </script>
 
 <template>
-  <main class="inlay">
-    <form @submit="onSubmit">
-      <FormField v-slot="{ username }" name="username">
-        <FormItem>
-          <FormControl>
-            <Input type="text" placeholder="Username" v-bind="username" />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <FormField v-slot="{ password }" name="password">
-        <FormItem>
-          <FormControl>
-            <Input type="password" placeholder="Passwort" v-bind="password" />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      </FormField>
-      <Button @click="test()" variant="outline">
-        Submit
-      </Button>
-    </Form>
+  <main class="inlay bg-[url(../../public/login-background-2.svg)] h-screen bg-no-repeat bg-cover flex items-center">
+    <div class="w-full">
+      <div>
+        <h1 class="font-mentra text-[64px] text-center">Hay</h1>
+      </div>
+      <div class="flex flex-col gap-6 items-center">
+        <div class="bg-white rounded-2xl bg-white p-6 drop-shadow-md w-full">
+          <form @submit="onSubmit" class="flex flex-col gap-4">
+            <FormField v-slot="{ username }" name="username">
+              <FormItem>
+                <FormControl class="border-none">
+                  <v-text-field variant="underlined"  placeholder="Username" v-bind="username"/>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ password }" name="password">
+              <FormItem>
+                <FormControl class="border-none">
+                  <v-text-field variant="underlined" type="password" placeholder="Passwort" v-bind="password"/>
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            </FormField>
+          </Form>
+        </div>
+        <Button @click="test()" variant="outline" class="w-fit px-16 py-6 drop-shadow-md rounded-full text-5 bg-white border-none">
+          Submit
+        </Button>
+      </div>
+    </div>
+
   </main>
 </template>
