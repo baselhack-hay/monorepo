2c6d8a438b631184df1e90d6a7c71a3d7f59d8b9
yveswehrli <yves.wehrli@bluewin.ch>
2023-10-28 15:39:50 +0200

    Add user create/list/delete endpoint

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
