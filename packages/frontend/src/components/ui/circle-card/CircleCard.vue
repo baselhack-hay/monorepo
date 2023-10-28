bca32d00270edc9a4684690ac454f26e6da64d43
mereditsommer <meredit.sommer@youengineering.ch>
2023-10-28 18:56:55 +0200

    add base for inner circles

---
+++ packages/frontend/src/components/ui/circle-card/CircleCard.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="font-yeseva text- text-6 rounded-2xl border border-black bg-white p-6 drop-shadow"
+  >
+    Groupname
+  </div>
+</template>
