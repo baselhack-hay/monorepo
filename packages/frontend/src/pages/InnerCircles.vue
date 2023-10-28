6125541480876d063b2e0c320951ded99892735d
megiSW <marco.inniger@sitewerk.ch>
2023-10-28 19:43:30 +0200

    fix: build errors

--- packages/frontend/src/pages/InnerCircles.vue
+++ packages/frontend/src/pages/InnerCircles.vue
@@ -4,8 +4,8 @@ import CircleCard from '@/components/ui/circle-card/CircleCard.vue'
 
 <template>
   <main class="bg-scared-light pt-[102px]">
-    <div class="inlay bg-scared-light flex flex-col gap-8">
-      <CircleCard v-for="n in 10"></CircleCard>
+    <div class="inlay flex flex-col gap-8 bg-scared-light">
+      <CircleCard v-for="_n in 10"></CircleCard>
     </div>
   </main>
 </template>
