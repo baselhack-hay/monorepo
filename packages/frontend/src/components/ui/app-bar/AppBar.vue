23635e1aaa3738db9bde7c769dba7fb229675948
samuellupica <samuel.lupica@students.fhnw.ch>
2023-10-28 19:41:27 +0200

    add background colour, set position sticky

--- packages/frontend/src/components/ui/app-bar/AppBar.vue
+++ packages/frontend/src/components/ui/app-bar/AppBar.vue
@@ -6,7 +6,7 @@ import router from '@/router';
 </script>
 
 <template>
-  <div class="fixed bottom-0 flex justify-between w-full rounded-t-2xl shadow-top">
+  <div class="sticky bg-white bottom-0 flex justify-between w-full rounded-t-2xl shadow-top">
     <div class="py-4 w-2/5 flex justify-center" @click="router.push('/')">
       <user-group-icon class="w-6"></user-group-icon>
     </div>
