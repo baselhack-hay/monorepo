90a8c2c6df34c199fd33bb74d453bc1bd313db7b
mereditsommer <meredit.sommer@youengineering.ch>
2023-10-29 09:49:24 +0100

    update inner circles page

--- packages/frontend/src/pages/Community.vue
+++ packages/frontend/src/pages/Community.vue
@@ -1,44 +1,51 @@
 <script setup lang="ts">
-import CommunityCard from "@/components/ui/communityCard/CommunityCard.vue";
-import {PlusIcon, XMarkIcon, PaperAirplaneIcon} from "@heroicons/vue/24/outline";
-import {onMounted, ref} from "vue";
+import CommunityCard from '@/components/ui/communityCard/CommunityCard.vue'
+import {
+  PlusIcon,
+  XMarkIcon,
+  PaperAirplaneIcon
+} from '@heroicons/vue/24/outline'
+import { onMounted, ref } from 'vue'
 import router from '@/router'
-import axios from "axios";
+import axios from 'axios'
 
 type Post = {
-  postId: number;
-  title: string;
-  description: string;
-  emotion: 'surprised' | 'happy' | 'scared' | 'sad' | 'angry' | 'rejecting';
+  postId: number
+  title: string
+  description: string
+  emotion: 'surprised' | 'happy' | 'scared' | 'sad' | 'angry' | 'rejecting'
 }
 
-const dialog = ref(false);
-const textareaValue = ref('');
-const titleValue = ref('');
-const posts = ref<Post[]>([]);
+const dialog = ref(false)
+const textareaValue = ref('')
+const titleValue = ref('')
+const posts = ref<Post[]>([])
 
 onMounted(async () => {
-  await getAllPosts();
-});
+  await getAllPosts()
+})
 
 const createPost = async () => {
-  if(titleValue.value.length > 0 && textareaValue.value.length > 0){
+  if (titleValue.value.length > 0 && textareaValue.value.length > 0) {
     try {
-      await axios.post(`${import.meta.env.VITE_BACKEND_HOST}/community/post`, {
-        title: titleValue.value,
-        description: textareaValue.value
-      }, {
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      withCredentials: true
-      })
-      await getAllPosts();
-      titleValue.value = '';
-      textareaValue.value = '';
-      dialog.value = false;
-    }
-    catch (error){
+      await axios.post(
+        `${import.meta.env.VITE_BACKEND_HOST}/community/post`,
+        {
+          title: titleValue.value,
+          description: textareaValue.value
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: true
+        }
+      )
+      await getAllPosts()
+      titleValue.value = ''
+      textareaValue.value = ''
+      dialog.value = false
+    } catch (error) {
       console.log(error)
     }
   }
@@ -46,25 +53,33 @@ const createPost = async () => {
 
 const getAllPosts = async () => {
   try {
-    const result = await axios.get(`${import.meta.env.VITE_BACKEND_HOST}/community/post`, {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      withCredentials: true
-    })
-    posts.value = result.data.reverse();
-  } catch (error){
-  }
+    const result = await axios.get(
+      `${import.meta.env.VITE_BACKEND_HOST}/community/post`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+    )
+    posts.value = result.data.reverse()
+  } catch (error) {}
 }
-
 </script>
 
 <template>
   <main>
     <div class="inlay flex flex-col gap-y-6">
-      <CommunityCard v-for="post in posts" @click="router.push(`/posts/${post.postId}`)" :id="post.postId" :title="post.title" :content="post.description" :variant="post.emotion"></CommunityCard>
+      <CommunityCard
+        v-for="post in posts"
+        @click="router.push(`/posts/${post.postId}`)"
+        :id="post.postId"
+        :title="post.title"
+        :content="post.description"
+        :variant="post.emotion"
+      ></CommunityCard>
     </div>
-    <div class=" fixed bottom-20 w-full flex flex-row-reverse px-6">
+    <div class="fixed bottom-20 flex w-full flex-row-reverse px-6">
       <div class="rounded-xl bg-black p-3 text-center" v-if="!dialog">
         <plus-icon @click="dialog = true" class="w-8 text-white"></plus-icon>
       </div>
@@ -72,15 +87,15 @@ const getAllPosts = async () => {
     <v-dialog
         v-model="dialog"
     >
-      <div class="flex bg-white rounded-lg p-4 flex-column gap-y-4">
-        <div class="w-full flex justify-between">
-          <x-mark-icon class="w-6" @click="dialog = false"/>
+      <div class="flex-column flex gap-y-4 rounded-lg bg-white p-4">
+        <div class="flex w-full justify-between">
+          <x-mark-icon class="w-6" @click="dialog = false" />
         </div>
-        <v-text-field v-model="titleValue" label="Title" class="font-poppins"/>
+        <v-text-field v-model="titleValue" label="Title" class="font-poppins" />
         <div>
-          <v-textarea v-model="textareaValue" label="Content"/>
-          <div class="w-full flex justify-between flex-row-reverse">
-            <paper-airplane-icon class="w-6" @click="createPost"/>
+          <v-textarea v-model="textareaValue" label="Content" />
+          <div class="flex w-full flex-row-reverse justify-between">
+            <paper-airplane-icon class="w-6" @click="createPost" />
           </div>
         </div>
       </div>
--- packages/frontend/src/pages/InnerCircles.vue
+++ packages/frontend/src/pages/InnerCircles.vue
@@ -1,13 +1,68 @@
 <script setup lang="ts">
 import CircleCard from '@/components/ui/circle-card/CircleCard.vue'
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+onMounted(async () => {
+  await getAllCirclesOfUsers()
+})
+
+type Circle = {
+  groupId: number
+  name: string
+}
+
+const circles = ref<Circle[]>([])
+
+const getAllCirclesOfUsers = async () => {
+  try {
+    const userId = await getCurrentUserId()
+    console.log(userId)
+    const result = await axios.get(
+      `${import.meta.env.VITE_BACKEND_HOST}/circle/group/user/${userId}`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+    )
+    circles.value = result.data.reverse()
+  } catch (error) {}
+}
+
+const getCurrentUserId = async () => {
+  try {
+    const result = await axios.get(
+      `${import.meta.env.VITE_BACKEND_HOST}/user/current`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+    )
+    return result.data
+  } catch (error) {}
+}
 </script>
 
 <template>
   <main>
-    <div class="inlay flex flex-col gap-y-6">
-      <CircleCard id="1" group-name="Friends"></CircleCard>
-      <CircleCard id="2" group-name="Family"></CircleCard>
-      <CircleCard id="3" group-name="Nadia"></CircleCard>
+    <div class="inlay">
+      <div v-if="circles.length > 0" class="flex flex-col gap-y-6">
+        <CircleCard
+          v-for="circle in circles"
+          :id="circle.groupId"
+          :group-name="circle.name"
+        ></CircleCard>
+      </div>
+      <div
+        v-else
+        class="font-yeseva text-5 rounded-2xl bg-white p-4 drop-shadow"
+      >
+        Trete einem Circle bei oder erstelle einen neuen.
+      </div>
     </div>
   </main>
 </template>
