533eb21127f1c022146e8b7e07f4e2d0650233a5
megiSW <marco.inniger@sitewerk.ch>
2023-10-29 13:52:48 +0100

    fix: image issues

--- packages/frontend/tailwind.config.ts
+++ packages/frontend/tailwind.config.ts
@@ -69,7 +69,7 @@ module.exports = {
         top: '0px 0px 15px 5px rgba(0, 0, 0, 0.3)'
       },
       backgroundImage: {
-        colorful: 'url(../../public/colorful-background.svg)'
+        colorful: 'url(/colorful-background.svg)'
       }
     }
   },
