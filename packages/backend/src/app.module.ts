120f88356d8b29bff4a19ed177c604fe99cee1e9
yveswehrli <yves.wehrli@bluewin.ch>
2023-10-29 10:35:55 +0100

    Revert "Try to fix dev login"
    This reverts commit c4915a3f7121b1455d457d7f12a6bad1daec79ed.

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -7,7 +7,6 @@ import { CommentModule } from './community/comment/comment.module';
 import { GroupModule } from './circle/group/group.module';
 import { QuestionModule } from './circle/question/question.module';
 import { AnswersModule } from './circle/answers/answers.module';
-import { ChatModule } from './circle/chat/chat.module';
 
 @Module({
   imports: [
@@ -19,7 +18,6 @@ import { ChatModule } from './circle/chat/chat.module';
     CommentModule,
     GroupModule,
     AnswersModule,
-    ChatModule,
   ],
   controllers: [],
   providers: [],
