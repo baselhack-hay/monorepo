ce936145983eab7215d080026a09c6a25a66d7d5
yveswehrli <yves.wehrli@bluewin.ch>
2023-10-29 14:28:25 +0100

    Update main.ts

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       secret: 'my-secret',
       resave: false,
       saveUninitialized: false,
-      cookie: { secure: false },
+      cookie: { secure: true },
     }),
   );
   app.use(passport.initialize());
