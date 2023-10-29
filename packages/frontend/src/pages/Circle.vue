043753c814b9a9ff86d732273b785e093f2aa927
cyrilkym <cyril.kym@youengineering.ch>
2023-10-29 13:19:42 +0100

    basic chat implemented

--- packages/frontend/src/pages/Circle.vue
+++ packages/frontend/src/pages/Circle.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import CommunityCard from '@/components/ui/communityCard/CommunityCard.vue'
-import { onMounted, ref } from 'vue'
+import {onMounted, onUnmounted, ref} from 'vue'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
+import { PaperAirplaneIcon } from "@heroicons/vue/24/outline";
 
 const router = useRouter()
 
@@ -17,10 +18,56 @@ type Answer = {
 }
 
 const answers = ref<Answer[]>([])
+const messages = ref<{username: string, message: string, userId: number}[]>([]);
+const currentInputValue = ref('')
+const userId = ref<number>(0)
 
-onMounted(async () => {
-  await getAnswers()
-})
+const sendMessage = async (message: string) => {
+  axios.post(`${import.meta.env.VITE_BACKEND_HOST}/circle/chat/${groupId}`,
+      {
+        message
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+  )
+}
+
+const getCurrentUser = async () =>{
+  const user = await axios.get(
+      `${import.meta.env.VITE_BACKEND_HOST}/user/current`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+  )
+  userId.value = user.data
+}
+
+const getChat = async () => {
+  const chat = await axios.get(
+      `${import.meta.env.VITE_BACKEND_HOST}/circle/chat/${groupId}`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true
+      }
+  )
+  messages.value = chat.data.map(message => {
+    return {
+      username: message.user.username,
+      message: message.message,
+      userId: message.user.userId
+    }
+  })
+  console.log("chat", messages.value);
+}
 
 const getAnswers = async () => {
   try {
@@ -50,6 +97,15 @@ const getAnswers = async () => {
   } catch (error) {}
 }
 
+onMounted(async () => {
+  await sendMessage("hello there")
+  await getCurrentUser();
+  setInterval(() => {
+    getChat()
+  }, 2000)
+})
+
+
 const getCircle = async () => {
   try {
     const result = await axios.get(
@@ -76,6 +132,19 @@ const getCircle = async () => {
         :content="answer.answer"
         :variant="answer.emotion"
       ></CommunityCard>
+      <div class="flex flex-col-reverse">
+        <div v-for="message in messages" class="w-full">
+          <div v-if="message.userId === userId" class="flex flex-row">
+            <p>{{message.username}}: {{message.message}}</p>
+          </div>
+          <div v-if="message.userId !== userId" class="flex flex-row-reverse">
+            <p>{{message.username}}: {{message.message}}</p>
+          </div>
+        </div>
+      </div>
+      <v-text-field v-model="currentInputValue"></v-text-field>
+      <paper-airplane-icon @click="sendMessage(currentInputValue)" class="w-6"/>
+
     </div>
   </main>
 </template>
