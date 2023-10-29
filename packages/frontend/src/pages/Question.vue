d37d959405f6841b39883ae390105a88a16a952b
leonden <leon.hochwimmer@coop.ch>
2023-10-29 07:10:56 +0100

    Question-card to question page

--- packages/frontend/src/pages/Question.vue
+++ packages/frontend/src/pages/Question.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import QuestionInput from '@/components/ui/question-input/QuestionInput.vue'
+import QuestionCard from '@/components/ui/question-card/QuestionCard.vue'
 </script>
 
 <template>
+  <QuestionCard class="mt-4"></QuestionCard>
   <QuestionInput></QuestionInput>
 </template>
