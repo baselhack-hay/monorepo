53083dfc2d47aa43878b84bcbd9f09c71382e89d
Poisonlocket <loribono@gmx.ch>
2023-10-29 08:28:07 +0100

    Implemented answer Entity

--- packages/backend/src/circle/answers/entities/answer.entity.ts
+++ packages/backend/src/circle/answers/entities/answer.entity.ts
@@ -1,1 +1,31 @@
-export class Answer {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../../user/entities/user.entity';
+@Entity()
+export class Answer {
+  @PrimaryGeneratedColumn()
+  answerId: number;
+
+  @Column({ length: 512 })
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  userId: number;
+
+  @OneToOne((type) => User)
+  @JoinColumn([{ name: 'userId', referencedColumnName: 'userId' }])
+  User: User;
+}
